8034f90425eda464f1054e58dc4e40f7a86fa172
TaiLe1101 <taic21a.th1@gmail.com>
2023-05-29 21:39:02 +0700

    oke

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import process from 'process';
 import bodyParser from 'body-parser';
@@ -11,6 +11,7 @@ import connectDb from './configs/connectDb';
 import logger from './helpers/logger';
 import connectCookieStore from './configs/connectCookieStore';
 import logENV from './helpers/logENV';
+import { responseData } from './helpers';
 
 dotenv.config();
 
@@ -35,6 +36,10 @@ connectCookieStore();
 route(app);
 
 logENV();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json(responseData(null, 'Connect to server successfully'));
+});
 
 app.listen(port, () => {
     logger.info(`⚡️[server]: Server is running at http://localhost:${port}`);
